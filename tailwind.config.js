41bebcc96730c7cb8aaf5baa2610becaad7ab3c3
lang tien <langtienk4@gmail.com>
2024-05-30 17:18:36 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{jsx, js}", "./src/index.js"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
