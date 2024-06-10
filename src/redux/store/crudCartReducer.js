6f99595e57fc593b083fb7cb6128f7d9fc219926
lang tien <langtienk4@gmail.com>
2024-06-10 22:20:32 +0700

    In the project directory

--- src/redux/store/crudCartReducer.js
+++ src/redux/store/crudCartReducer.js
@@ -1,43 +1,6 @@
 const initialState = {
-  total: 5,
-  data: [
-    {
-      id: "48",
-      quantity: 2,
-      name: "Xiaomi Redmi Note 12 Pro 5G",
-      img_url:
-        "https://cellphones.com.vn/media/catalog/product/x/i/xiaomi-redmi-note-12-pro-5g.png",
-      priceShow: 7390000,
-      priceThrought: 9490000,
-    },
-    {
-      id: "130",
-      quantity: 1,
-      name: "Laptop Lenovo Ideapad Slim 5 14IMH9 83DA0020VN",
-      img_url:
-        "https://cellphones.com.vn/media/catalog/product/l/a/laptop-lenovo-ideapad-slim-5-14imh9-83da0020vn-thumbnails.png",
-      priceShow: 26190000,
-      priceThrought: 27990000,
-    },
-    {
-      id: "316",
-      quantity: 1,
-      name: "Tai nghe Bluetooth True Wireless SoundPEATS Capsule 3 Pro",
-      img_url:
-        "https://cellphones.com.vn/media/catalog/product/g/r/group_116_2.png",
-      priceShow: 940000,
-      priceThrought: 1690000,
-    },
-    {
-      id: "55",
-      quantity: 1,
-      name: "OPPO Reno11 F 5G 8GB 256GB",
-      img_url:
-        "https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-oppo-reno-11-f-2.png",
-      priceShow: 8490000,
-      priceThrought: 8990000,
-    },
-  ],
+  total: 0,
+  data: [],
 };
 
 const crudCartReducer = (state = initialState, action) => {
